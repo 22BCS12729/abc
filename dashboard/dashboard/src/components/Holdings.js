5032486486964f2db51131e4331237027b73fd43
unknown <your-email@example.com>
2025-07-10 14:40:57 +0530

    deployed backend

--- dashboard/dashboard/src/components/Holdings.js
+++ dashboard/dashboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allHoldings").then((res) => {
+    axios.get("https://abc-rp91.onrender.com/allHoldings").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
